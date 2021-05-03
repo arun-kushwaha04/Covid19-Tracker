379dcf36d0777ac723c1c048df1320fdd66b5057
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-05-03 09:45:09 +0530

    Update state.js

--- JS/state.js
+++ JS/state.js
@@ -1,11 +1,28 @@
 let currentTime = new Date();
+let year = currentTime.getFullYear();
 let month = currentTime.getMonth() + 1;
-if (month < 10) month = '0' + month;
 let date = currentTime.getDate() - 1;
+if (date == 0) {
+    month -= 1;
+    if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) date = 31;
+    else if (month == 2) {
+        if (year % 400 == 0 || (yaer % 4 == 0 && year % 100 != 0)) { date = 29; } else { date = 28; }
+    } else date = 30;
+}
+if (month == 0) {
+    month = 30;
+    date = 31;
+    year -= 1;
+}
+if (month < 10) month = '0' + month;
 if (date < 10) date = '0' + date;
 
-const currdate = `${currentTime.getFullYear()}-${month}-${date}`
+
+const currdate = `${year}-${month}-${date}`
 const stateUrl = `https://api.covid19api.com/live/country/india/status/confirmed/date/${currdate}T00:00:00Z`
+
+let h2 = document.querySelector('.text').children[1];
+h2.textContent = `As on ${currdate}`;
 
 const Container = document.querySelector('.Container');
 fetch(stateUrl).then((cases) => {
